549285db6edf61b445139ebf811a87fff56e221a
Naveen <vnaveenlgp2001@gmail.com>
2024-05-27 10:23:11 +0530

    chat card designs

--- components/lists/chats/index.tsx
+++ components/lists/chats/index.tsx
@@ -12,6 +12,8 @@ import Avatar from "@mui/material/Avatar";
 import DoneAllIcon from '@mui/icons-material/DoneAll';
 import PhotoCameraIcon from '@mui/icons-material/PhotoCamera';
 import PushPinIcon from '@mui/icons-material/PushPin';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import moment from "moment";
 interface ChatsProps {
     tab: string;
 }
@@ -20,12 +22,15 @@ const Chats: React.FC<ChatsProps> = ({ tab }) => {
     const [searchTerm, setSearchTerm] = useState('')
     const [tag, setTag] = useState('all')
     const [isFocused, setIsFocused] = useState(false);
+    const [hover, setHover] = useState({ 'hover': false, 'name': '' });
     const inputRef = useRef<HTMLInputElement>(null);
+
     const handleDivClick = () => {
         if (inputRef?.current) {
             inputRef?.current?.focus();
         }
     };
+
     const handleFocus = () => {
         setIsFocused(true);
     };
@@ -51,7 +56,23 @@ const Chats: React.FC<ChatsProps> = ({ tab }) => {
         return 'Select'
     }
 
-    // console.log(onClick)
+    function handleChats() {
+        const latestTimestamps: { [key: string]: string } = {};
+        ChatDetails.messages.forEach(message => {
+            const sender = message.sender;
+            const timestamp = message.timestamp;
+            if (!latestTimestamps[sender] || new Date(timestamp) > new Date(latestTimestamps[sender])) {
+                latestTimestamps[sender] = timestamp;
+            }
+        });
+        ChatDetails.participants.sort((a, b) => {
+            const timestampA = latestTimestamps[a.id];
+            const timestampB = latestTimestamps[b.id];
+            return new Date(timestampB).getTime() - new Date(timestampA).getTime();
+        });
+        return ChatDetails;
+    }
+
     return (
         <div>
             <div className="sticky top-0">
@@ -92,11 +113,11 @@ const Chats: React.FC<ChatsProps> = ({ tab }) => {
                             <PhotoCameraIcon className="text-[#8696a0]" />
                             <div className="text-[#3b4a54]">Photo</div>
                         </div>
-                        <div><PushPinIcon className="text-[#8696a0]"/></div>
+                        <div><PushPinIcon className="text-[#8696a0]" /></div>
                     </div>
                 </div>
             </div>
-            <div className="grid grid-cols-12 items-center pl-4 py-3 bg-white">
+            {/* <div className="grid grid-cols-12 items-center pl-4 pt-3 bg-white hover:bg-[#f5f6f6]" onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
                 <div className="col-span-2 items-center">
                     <Avatar alt="Naveen" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQP3FqoYVPZxd_5TH71L8ccH4Mf7CxqWD0OxnPOL8_3bg&s" sx={{ width: 50, height: 50 }} />
                 </div>
@@ -111,12 +132,13 @@ const Chats: React.FC<ChatsProps> = ({ tab }) => {
                             <PhotoCameraIcon className="text-[#8696a0]" />
                             <div className="text-[#3b4a54]">Photo</div>
                         </div>
+                        {hover ? <div><KeyboardArrowDownIcon className="text-[#8696a0]" /></div> : <div></div>}
                     </div>
                 </div>
                 <div className="col-span-2"></div>
                 <div className="col-span-10 pt-4 border-b border-[#e9edef]"></div>
-            </div>
-            <div className="grid grid-cols-12 items-center pl-4 pb-3 bg-white">
+            </div> */}
+            <div className="grid grid-cols-12 items-center pl-4 pt-3 bg-white hover:bg-[#f5f6f6]">
                 <div className="col-span-2 items-center">
                     <Avatar alt="Naveen" src="" sx={{ width: 50, height: 50 }} />
                 </div>
@@ -134,6 +156,34 @@ const Chats: React.FC<ChatsProps> = ({ tab }) => {
                 <div className="col-span-2"></div>
                 <div className="col-span-10 pt-4 border-b border-[#e9edef]"></div>
             </div>
+            {handleChats()?.participants?.map((each, index) => {
+                return (
+                    <div key={index} className="grid grid-cols-12 items-center cursor-pointer pl-4 pt-3 bg-white hover:bg-[#f5f6f6]" onMouseEnter={() => setHover({ name: each?.name, hover: true })} onMouseLeave={() => setHover({ name: each?.name, hover: false })}>
+                        <div className="col-span-2 items-center">
+                            <Avatar alt="Naveen" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQP3FqoYVPZxd_5TH71L8ccH4Mf7CxqWD0OxnPOL8_3bg&s" sx={{ width: 50, height: 50 }} />
+                        </div>
+                        <div className="col-span-9 items-center">
+                            <div className="flex justify-between items-center">
+                                <div className="text-[#111b21] text-[17px]">{each?.name}</div>
+                                <div className="text-[#667781] text-xs">{moment(ChatDetails?.messages?.find((msg) => msg.sender == each.id)?.timestamp).format('LT')}</div>
+                            </div>
+                            <div className="flex justify-between items-center">
+                                <div className="flex items-center gap-1">
+                                    <DoneAllIcon className="text-[#53bdeb]" />
+                                    <PhotoCameraIcon className="text-[#8696a0]" />
+                                    <div className="text-[#3b4a54]">Photo</div>
+                                </div>
+                                <div className="flex">
+                                    {/* <PushPinIcon className="text-[#8696a0]" /> */}
+                                    <KeyboardArrowDownIcon className={`${hover?.name == each?.name && hover?.hover ? 'block' : 'hidden'} text-[#8696a0]`} />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-span-2"></div>
+                        <div className="col-span-10 pt-4 border-b border-[#e9edef]"></div>
+                    </div>
+                )
+            })}
         </div>
     )
 }
