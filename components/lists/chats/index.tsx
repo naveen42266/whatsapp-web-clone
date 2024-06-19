b198d9a56322e6e1f81e94c63fefe73a7666e653
Naveen <vnaveenlgp2001@gmail.com>
2024-06-19 20:12:14 +0530

    chat screen alterations

--- components/lists/chats/index.tsx
+++ components/lists/chats/index.tsx
@@ -11,10 +11,10 @@ import { ChatDetails } from "../../../json";
 import Avatar from "@mui/material/Avatar";
 import DoneAllIcon from '@mui/icons-material/DoneAll';
 import PhotoCameraIcon from '@mui/icons-material/PhotoCamera';
-import PushPinIcon from '@mui/icons-material/PushPin';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import VideocamIcon from '@mui/icons-material/Videocam';
 import DescriptionIcon from '@mui/icons-material/Description';
+import PushPinIcon from '@mui/icons-material/PushPin';
 import moment from "moment";
 interface ChatsProps {
     tab: string;
@@ -23,6 +23,7 @@ interface ChatsProps {
 const Chats: React.FC<ChatsProps> = ({ tab, handleChatUser }) => {
     const tags = ['all', 'unread', 'groups']
     const [searchTerm, setSearchTerm] = useState<string>('')
+    const [person, setPerson] = useState<string>('')
     const [tag, setTag] = useState<string>('all')
     const [isFocused, setIsFocused] = useState<boolean>(false);
     const [hover, setHover] = useState({ 'hover': false, 'name': '' });
@@ -148,8 +149,6 @@ const Chats: React.FC<ChatsProps> = ({ tab, handleChatUser }) => {
                     <div className="text-[22px] font-bold capitalize">{tab}</div>
                     <div className="relative flex items-center gap-6 mr-1">
                         <div className="cursor-pointer"><svg color="#54656f" height="24" width="24" fill="none"><title>new-chat-outline</title><path d="M9.53277 12.9911H11.5086V14.9671C11.5086 15.3999 11.7634 15.8175 12.1762 15.9488C12.8608 16.1661 13.4909 15.6613 13.4909 15.009V12.9911H15.4672C15.9005 12.9911 16.3181 12.7358 16.449 12.3226C16.6659 11.6381 16.1606 11.0089 15.5086 11.0089H13.4909V9.03332C13.4909 8.60007 13.2361 8.18252 12.8233 8.05119C12.1391 7.83391 11.5086 8.33872 11.5086 8.991V11.0089H9.49088C8.83941 11.0089 8.33411 11.6381 8.55097 12.3226C8.68144 12.7358 9.09947 12.9911 9.53277 12.9911Z" fill="currentColor"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M0.944298 5.52617L2.99998 8.84848V17.3333C2.99998 18.8061 4.19389 20 5.66665 20H19.3333C20.8061 20 22 18.8061 22 17.3333V6.66667C22 5.19391 20.8061 4 19.3333 4H1.79468C1.01126 4 0.532088 4.85997 0.944298 5.52617ZM4.99998 8.27977V17.3333C4.99998 17.7015 5.29845 18 5.66665 18H19.3333C19.7015 18 20 17.7015 20 17.3333V6.66667C20 6.29848 19.7015 6 19.3333 6H3.58937L4.99998 8.27977Z" fill="currentColor"></path></svg></div>
-                        {/* <div><ChatBubbleOutlineIcon className="rotate-90" /></div>
-                    <div className="absolute top-0 left-1.5 right-0 bottom-4 pb-4 text-base font-bold">+</div> */}
                         <div className="cursor-pointer"><MoreVertIcon className="text-[#54656f]" /></div>
                     </div>
                 </div>
@@ -227,7 +226,7 @@ const Chats: React.FC<ChatsProps> = ({ tab, handleChatUser }) => {
             <div className="h-[74%] w-full overflow-y-scroll custom-scroll">
                 {handleChats()?.participants?.map((each, index) => {
                     return (
-                        <div key={index} className="grid grid-cols-12 items-center cursor-pointer pl-4 pt-3 bg-white hover:bg-[#f5f6f6]" onMouseEnter={() => setHover({ name: each?.name, hover: true })} onMouseLeave={() => setHover({ name: each?.name, hover: false })} onClick={() => { handleChatUser(each?.name) }}>
+                        <div key={index} className={`grid grid-cols-12 items-center cursor-pointer pl-4 pt-3 ${person==each.name ? 'bg-[#f0f2f5]' : 'bg-white hover:bg-[#f5f6f6]'} `} onMouseEnter={() => setHover({ name: each?.name, hover: true })} onMouseLeave={() => setHover({ name: each?.name, hover: false })} onClick={() => { handleChatUser(each?.name),setPerson(each?.name) }}>
                             <div className="col-span-2 items-center">
                                 <Avatar alt="Naveen" src={ChatDetails?.messages?.find((msg) => msg.sender === each.id)?.profile ?? ''} sx={{ width: 50, height: 50 }} />
                             </div>
