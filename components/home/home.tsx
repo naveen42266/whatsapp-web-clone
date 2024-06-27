9ea9111cb3677418eae7cf2c175e09d044195725
Naveen <vnaveenlgp2001@gmail.com>
2024-06-27 17:42:35 +0530

    setting - chats-wallpaper screens , empty home screen

--- components/home/home.tsx
+++ components/home/home.tsx
@@ -10,13 +10,16 @@ export default function Home() {
     const [selectedTab, setSelectedTab] = useState<string>('chats');
     const { whatsapp, setWhatsapp } = useContext<any>(WhatsappContext)
     function handleDetailScreen() {
-        if ((whatsapp?.tabSection?.title == 'chat' || whatsapp?.tab == 'chats') && whatsapp?.tabSection?.user !== '' ) {
+        if ((whatsapp?.tabSection?.title == 'chat' || whatsapp?.tab == 'chats') && whatsapp?.tabSection?.user !== '') {
             return <ChatDetails user={whatsapp?.tabSection?.user || ''} />
         }
-        // else if (whatsapp?.wallpaper?.isWallpaper) {
-        //     return <div className="bgImg"></div>
-        // }
-        return <DetailComponent />
+        else if ((whatsapp?.tabSection?.title !== 'chat' || whatsapp?.tab !== 'chats') && whatsapp?.wallpaper?.isWallpaper) {
+            return <div className={`flex flex-col justify-between h-full w-full ${whatsapp?.wallpaper?.bgImage}`}>
+                <div className="bg-[#f0f2f5] h-16 w-full flex justify-center items-center">Wallpaper preview</div>
+                <div className="bg-[#f0f2f5] h-16 w-full"></div>
+            </div>
+        }
+        return <div className="bg-[#EFEAE2] h-full"><DetailComponent /></div>
     }
 
     function handleSetTabDetails(title: string, user: string, mobile: string) {
@@ -42,7 +45,7 @@ export default function Home() {
                     </div>
                     <div className="h-[100%] w-[88%] bg-white"><ListComponent tab={selectedTab} handleDetailContent={(value: string, mobile: string, key: string) => { handleSetTabDetails(key, value, mobile) }} /></div>
                 </div>
-                <div className={`h-[100%] hidden sm:block w-0 ${whatsapp?.userProfile ? 'sm:w-[32%] md:w-[36%]' : 'sm:w-[60%] md:w-[66%] '} ${whatsapp?.tabSection?.title == 'chat' ? whatsapp?.wallpaper?.bgImage : ''}`} style={{backgroundColor: whatsapp?.wallpaper?.hoverColor ? whatsapp?.wallpaper?.hoverColor : whatsapp?.wallpaper?.color}}> {/*${whatsapp?.tabSection?.title == 'chat' ? 'bgImg' : ''}*/}
+                <div className={`h-[100%] hidden sm:block w-0 ${whatsapp?.userProfile ? 'sm:w-[32%] md:w-[36%]' : 'sm:w-[60%] md:w-[66%] '} ${whatsapp?.tabSection?.title == 'chat' && whatsapp?.wallpaper?.bgImage}`} style={{ backgroundColor: whatsapp?.wallpaper?.hoverColor ? whatsapp?.wallpaper?.hoverColor : whatsapp?.wallpaper?.color }}> {/*${whatsapp?.tabSection?.title == 'chat' ? 'bgImg' : ''}*/}
                     {handleDetailScreen()}
                 </div>
                 <div className={`h-[100%] sm:w-[28%] md:w-[30%] ${whatsapp?.userProfile ? 'block' : "hidden"}`}>
