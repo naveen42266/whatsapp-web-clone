2d5df175fadf5605f98d0fa55cfd88ba30f11d73
Naveen <vnaveenlgp2001@gmail.com>
2024-07-06 19:11:37 +0530

    chat features

--- components/detail/chat/index.tsx
+++ components/detail/chat/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import Avatar from "@mui/material/Avatar";
 import SearchIcon from '@mui/icons-material/Search';
@@ -6,6 +6,10 @@ import VideocamIcon from '@mui/icons-material/Videocam';
 import AddIcon from '@mui/icons-material/Add';
 import MicIcon from '@mui/icons-material/Mic';
 import TextField from "@mui/material/TextField";
+import SendIcon from '@mui/icons-material/Send';
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
+import moment from "moment";
+import EmojiPicker from "emoji-picker-react";
 import { WhatsappContext } from "../../../useContext";
 
 interface ChatDetailsProps {
@@ -66,7 +70,11 @@ const metaAi = [
     { sender: 'other', text: 'Sure thing! 👍', time: '2:30 PM' },
 ];
 const ChatDetails: React.FC<ChatDetailsProps> = ({ user }) => {
-    const { whatsapp, setWhatsapp } = React.useContext<any>(WhatsappContext)
+    const { whatsapp, setWhatsapp } = React.useContext<any>(WhatsappContext);
+    const [list, setList] = useState(messages);
+    const [message, setMessage] = useState('');
+    const [emojiPickerVisible, setEmojiPickerVisible] = useState({ type: "", id: "", isEmoji: false });
+    const [isHovered, setIsHovered] = useState({ type: "", id: "" })
 
     const isFirstMessageOfSender = (messages: { sender: any; }[], index: number) => {
         if (index === 0) return true;
@@ -76,8 +84,23 @@ const ChatDetails: React.FC<ChatDetailsProps> = ({ user }) => {
         setWhatsapp({ ...whatsapp, userProfile: true });
     }
 
+    function handleChat(chat: string) {
+        let newChat = { sender: '', text: '', time: '', edited: false };
+        newChat.sender = 'user';
+        newChat.text = chat;
+        newChat.time = moment(new Date()).format('LT');
+        list?.push(newChat);
+        setList(list);
+        setMessage('')
+    }
+
+    const onEmojiClick = (event: any, emojiObject: any) => {
+        // setMessage(message + emojiObject.emoji as string);
+        setEmojiPickerVisible({ ...emojiPickerVisible, type: '', id: '', isEmoji: false });
+    };
+    console.log(isHovered, 'emojiPickerVisible' , emojiPickerVisible)
     return (
-        <div className={`h-screen w-full`} > {/*style={{ backgroundImage: 'url(https://static.whatsapp.net/rsrc.php/v3/yl/r/gi_DckOUM5a.png)' }}*/}
+        <div className={`h-screen w-full`} onClick={() => { emojiPickerVisible?.isEmoji && setEmojiPickerVisible({ ...emojiPickerVisible, type: '', id: '', isEmoji: false }); }} > {/*style={{ backgroundImage: 'url(https://static.whatsapp.net/rsrc.php/v3/yl/r/gi_DckOUM5a.png)' }}*/}
             <div className=" h-[7.5%] bg-[#f0f2f5] p-2 flex justify-between cursor-pointer" onClick={() => { handleUserDetails() }}>
                 <div className="flex items-center gap-3">
                     <Avatar alt="Naveen" src={''} sx={{ width: "42px", height: "42px" }} />
@@ -97,13 +120,20 @@ const ChatDetails: React.FC<ChatDetailsProps> = ({ user }) => {
                     <span className="text-[#54656f] text-sm px-2 py-1 rounded-md shadow-md" style={{ backgroundColor: "rgba(255, 255, 255)" }}>TUESDAY</span>
                 </div>
                 <div className="p-6">
-                    {messages.map((message, index) => (
-                        <div key={index} className={`flex cursor-pointer  ${message.sender === 'user' ? 'justify-end' : 'justify-start'} mb-[3px]`}>
+                    {list.map((message, index) => (
+                        <div key={index} className={`flex cursor-pointer  ${message.sender === 'user' ? 'justify-end' : 'justify-start'} mb-[3px]`} onMouseEnter={() => { setIsHovered({ ...isHovered, type: message?.sender, id: `${index}` }) }} onMouseLeave={() => { setIsHovered({ ...isHovered, type: "", id: "" }) }}>
                             {message.sender !== 'user' && isFirstMessageOfSender(messages, index) ? (<div className={`w-0 h-0 rotate-180 border-t-[15px] border-t-transparent border-l-[15px] border-l-white`}></div>) : <div className="p-2"></div>}
+                            {message.sender === 'user' && isHovered?.type === 'user' && isHovered?.id === `${index}` && <EmojiEmotionsIcon className="text-white bg-[#bbbfc1] p-0.5 mt-3 mr-2 rounded-full" onClick={() => { setEmojiPickerVisible({ ...emojiPickerVisible, type: message?.sender, id: `${index}`, isEmoji: !emojiPickerVisible?.isEmoji }); }} />}
                             <div className={`rounded-lg px-3 py-1 shadow-md max-w-[65%] ${message.sender === 'user' ? 'bg-[#d9fdd3] rounded-tr-none' : 'bg-white rounded-tl-none'}`}>
                                 <p className="text-sm pr-16">{message.text}</p>
                                 <p className="text-xs text-gray-500 mt-1 text-right">{message.time} {message.edited ? '(Edited)' : ''}</p>
+                                {/* {emojiPickerVisible?.id === `${index}` && emojiPickerVisible?.type === message?.sender && emojiPickerVisible?.isEmoji && (
+                                    <div className="fixed top-20 left-0 right-0 z-20">
+                                        <EmojiPicker onEmojiClick={onEmojiClick} />
+                                    </div>
+                                )} */}
                             </div>
+                            {message.sender !== 'user' && isHovered?.type !== 'user' && isHovered?.id === `${index}` && <EmojiEmotionsIcon className="text-white bg-[#bbbfc1] p-0.5 mt-3 ml-2 rounded-full" onClick={() => { setEmojiPickerVisible({ ...emojiPickerVisible, type: message?.sender, id: `${index}`, isEmoji: !emojiPickerVisible?.isEmoji }); }} />}
                             {message.sender === 'user' && isFirstMessageOfSender(messages, index) ? (<div className={`w-0 h-0 rotate-90 border-t-[15px] border-t-transparent border-l-[15px] border-l-[#d9fdd3]`}></div>) : <div className="p-2"></div>}
                         </div>
                     ))}
@@ -112,8 +142,8 @@ const ChatDetails: React.FC<ChatDetailsProps> = ({ user }) => {
             <div className=" h-[7.5%] w-full bg-[#f0f2f5] p-2 flex items-center">
                 <div className="w-[5%] flex justify-center"> <svg color="#54656f" height="24" width="24" version="1.1" x="0px" y="0px" enable-background="new 0 0 24 24"><title>smiley</title><path fill="currentColor" d="M9.153,11.603c0.795,0,1.439-0.879,1.439-1.962S9.948,7.679,9.153,7.679 S7.714,8.558,7.714,9.641S8.358,11.603,9.153,11.603z M5.949,12.965c-0.026-0.307-0.131,5.218,6.063,5.551 c6.066-0.25,6.066-5.551,6.066-5.551C12,14.381,5.949,12.965,5.949,12.965z M17.312,14.073c0,0-0.669,1.959-5.051,1.959 c-3.505,0-5.388-1.164-5.607-1.959C6.654,14.073,12.566,15.128,17.312,14.073z M11.804,1.011c-6.195,0-10.826,5.022-10.826,11.217 s4.826,10.761,11.021,10.761S23.02,18.423,23.02,12.228C23.021,6.033,17.999,1.011,11.804,1.011z M12,21.354 c-5.273,0-9.381-3.886-9.381-9.159s3.942-9.548,9.215-9.548s9.548,4.275,9.548,9.548C21.381,17.467,17.273,21.354,12,21.354z  M15.108,11.603c0.795,0,1.439-0.879,1.439-1.962s-0.644-1.962-1.439-1.962s-1.439,0.879-1.439,1.962S14.313,11.603,15.108,11.603z"></path> </svg></div>
                 <div className="w-[5%] flex justify-center"><AddIcon className="text-[#54656f]" /></div>
-                <div className="bg-white w-[85%] rounded-lg flex items-center"> <TextField id="standard-basic" className="searchField ml-2 py-1" placeholder={'Type a message'} variant="standard" fullWidth InputProps={{ disableUnderline: true, }} /> </div>
-                <div className="w-[5%] flex justify-center pl-2"> <MicIcon className="text-[#54656f]" /> </div>
+                <div className="bg-white w-[85%] rounded-lg flex items-center"> <TextField id="standard-basic" className="searchField ml-2 py-1" placeholder={'Type a message'} variant="standard" fullWidth InputProps={{ disableUnderline: true, }} value={message} onChange={(evt) => { setMessage(evt?.target?.value) }} /> </div>
+                <div className="w-[5%] flex justify-center pl-2">{message ? <SendIcon className="text-[#54656f]" onClick={() => { handleChat(message) }} /> : <MicIcon className="text-[#54656f]" />} </div>
             </div>
         </div>
     )
